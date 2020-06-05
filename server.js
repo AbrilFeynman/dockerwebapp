196fe5668a2e09659e3476629ba02f4a58d73d0f
Abril De Feynman <43973597+AbrilFeynman@users.noreply.github.com>
2020-06-04 19:37:51 -0500

    mars
    mars

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world earth\n');
+  res.send('Hello world mars\n');
 });
 
 app.listen(PORT, HOST);
